cb92222563cff6f7aeb3843737e54522b4a6d8fc
mehuu2000 <mehuu2000@gmail.com>
2024-06-04 16:34:44 +0900

    多分できた

---
+++ static/js/addTag.js
@@ -0,0 +1,35 @@
+function addMemo() {
+    var inputCount = document.querySelectorAll('[id^="memo"]').length + 1;
+    var newMemoHTML = `
+      <div>
+        <label for="memo${inputCount}">メモ</label>
+        <input type="text" id="memo${inputCount}" name="memo${inputCount}">
+      </div>
+    `;
+    var memoContainer = document.getElementById('memoContainer');
+    var newMemoDiv = document.createElement('div');
+    newMemoDiv.innerHTML = newMemoHTML;
+    memoContainer.appendChild(newMemoDiv);
+  }
+  
+  function addTag() {
+    var inputCount = document.querySelectorAll('[id^="tag_name"]').length + 1;
+    var newTagHTML = `
+      <div>
+        <label for="tag_name${inputCount}">タグ</label>
+        <input type="text" id="tag_name${inputCount}" name="tag_name${inputCount}">
+        <label for="tag_color${inputCount}">色</label>
+        <select id="tag_color${inputCount}" name="tag_color${inputCount}">
+            <option value="黒">黒</option>
+            <option value="赤">赤</option>
+            <option value="緑">緑</option>
+            <option value="黄">黄</option>
+            <option value="青">青</option>
+        </select>
+      </div>
+    `;
+    var tagContainer = document.getElementById('tagContainer');
+    var newTagDiv = document.createElement('div');
+    newTagDiv.innerHTML = newTagHTML;
+    tagContainer.appendChild(newTagDiv);
+  }
