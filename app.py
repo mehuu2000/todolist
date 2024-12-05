661ec0adedf208506b00d8da5c999078e6681806
mehuu2000 <mehuu2000@gmail.com>
2024-12-05 18:48:27 +0900

    改良を加えました

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, url_for, flash
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
@@ -7,15 +7,20 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 import os
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todolist.db"
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30) #セッションの有効期限のsetteeい
 
 db = SQLAlchemy(app)
 login_manager=LoginManager()
 login_manager.init_app(app)
+
+login_manager.login_view = 'login'  # ログインページのエンドポイントを指定
+login_manager.login_message = "ログインしてください。" #ログイン失敗時のメッセージ内容
+login_manager.login_message_category = "info" #上のメッセージのカテゴリーを設定
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,26 +30,23 @@ class User(UserMixin, db.Model):
     
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    day = db.Column(db.Date, nullable=False)
+    day = db.Column(db.DateTime, nullable=False)
     do = db.Column(db.String, nullable=False)
-    memo = db.Column(db.String)
     User_link_id = db.Column(db.Integer, nullable=False)
     
-    important = db.Column(db.Boolean, default=False)
-    uni = db.Column(db.Boolean, default=False)
-    private = db.Column(db.Boolean, default=False)
-    task = db.Column(db.Boolean, default=False)
-    #user = db.relationship('User', backref=db.backref('todos', lazy=True))
-    
-'''
 class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    important = db.Column(db.Boolean, default=False)
-    uni = db.Column(db.Boolean, default=False)
-    private = db.Column(db.Boolean, default=False)
-    task = db.Column(db.Boolean, default=False)
+    tag_name = db.Column(db.String, nullable=False)
+    tag_color = db.Column(db.String, nullable=False)
+    User_link_id = db.Column(db.Integer, nullable=False)
+    Todo_link_id = db.Column(db.Integer, nullable=False) #Todoのタグの識別を数字で管理
+    
+class Memo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=False)
     Todo_link_id = db.Column(db.Integer, nullable=False)
-'''
+    
+    check_memo = db.Column(db.Boolean, default=False)
     
 with app.app_context():
     db.create_all()
@@ -52,6 +54,40 @@ with app.app_context():
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+#日付の編集
+@app.template_filter('datetime_format_full')
+def datetime_format_full(value, format='%Y-%m-%d %H:%M'):
+    if value is None:
+        return ""
+    return value.strftime(format)
+@app.template_filter('datetime_format_Y')
+def datetime_format_Y(value, format='%Y'):
+    if value is None:
+        return ""
+    return value.strftime(format)
+@app.template_filter('datetime_format_m')
+def datetime_format_m(value, format='%m'):
+    if value is None:
+        return ""
+    return value.strftime(format)
+@app.template_filter('datetime_format_d')
+def datetime_format_d(value, format='%d'):
+    if value is None:
+        return ""
+    return value.strftime(format)
+@app.template_filter('datetime_format_H')
+def datetime_format_H(value, format='%H'):
+    if value is None:
+        return ""
+    return value.strftime(format)
+@app.template_filter('datetime_format_M')
+def datetime_format_M(value, format='%M'):
+    if value is None:
+        return ""
+    return value.strftime(format)
+#----------------
     
     
 
@@ -72,8 +108,10 @@ def login():
         
         if user and password and check_password_hash(user.password, password):
             login_user(user)
-            return redirect('/home')
+            next_page = request.args.get('next')  # ← ここで next パラメータを取得
+            return redirect(next_page or url_for('home')) 
         else:
+            
             error = "※ error:登録されていない、もしくは名前かパスワードが違います ※"
             return render_template('login.html', error=error)
     
@@ -111,88 +149,130 @@ def signup():
 @login_required
 def home():
     todos = Todo.query.filter_by(User_link_id=current_user.id).order_by(Todo.day).all()
-    flag = 0
+    todos_with_tags = []
+    
+    for todo in todos:
+        tags = Tag.query.filter_by(User_link_id=current_user.id, Todo_link_id=todo.id).all()
+        todos_with_tags.append((todo, tags))
+        
     if request.method == 'POST':
-        title = request.form.get('title')
-        tag = request.form.get('tag')
-        if not title and not tag:
-            error="検索する対象を記入して下さい"
-            return render_template('home.html', todos=todos, flag=flag, error=error)
-        elif title and tag:
-            error="検索対象は一つにして下さい"
-            return render_template('home.html', todos=todos, flag=flag, error=error)
-        
-        if 'search_title' in request.form:
-            if title:
-                todos = Todo.query.filter_by(User_link_id=current_user.id).filter(Todo.do.contains(title)).order_by(Todo.day).all()
-                flag=1
-                
-                if not todos:
-                    todos = Todo.query.filter_by(User_link_id=current_user.id).order_by(Todo.day).all()
-                    error = "見つかりませんでした"
-                    flag=0
-                    
-                return render_template('home.html', todos=todos, flag=flag, error=error)
-        
-        elif 'search_tag' in request.form:
-            if tag:
-                flag = 1
-                error = ""
-                if tag == '重要':
-                    todos = Todo.query.filter_by(User_link_id=current_user.id, important=True).order_by(Todo.day).all()
-                elif tag == '大学':
-                    todos = Todo.query.filter_by(User_link_id=current_user.id, uni=True).order_by(Todo.day).all()
-                elif tag == 'プライベート':
-                    todos = Todo.query.filter_by(User_link_id=current_user.id, private=True).order_by(Todo.day).all()
-                elif tag == 'タスク':
-                    todos = Todo.query.filter_by(User_link_id=current_user.id, task=True).order_by(Todo.day).all()
-                else:
-                    error = "見つかりませんでした"
-                    flga = 0
-                
-                if not todos:
-                    error = "見つかりませんでした"
-                    flag=0
-                    
-                return render_template('home.html', todos=todos, flag=flag, error=error)
-            
-        else:
-            error = "見つかりませんでした"
-            return render_template('home.html', todos=todos, flag=flag, error=error)
+        return render_template('home.html')
+    
                 
     
     else:
-        return render_template('home.html', todos=todos, flag=flag)
+        return render_template('home.html', todos_with_tags=todos_with_tags)
+
+
+@app.route('/<int:id>', methods=['GET', 'POST'])
+@login_required
+def veiw(id):
+    todo = Todo.query.filter_by(id=id).first()
+    tags = Tag.query.filter_by(User_link_id=current_user.id, Todo_link_id=id).all()
+    memos = Memo.query.filter_by(Todo_link_id=id).all()
+    
+    if request.method == 'POST':
+        form_data = request.form
+        for memo in memos:
+            checkbox_state = form_data.get(f'memo_check{memo.id}')
+            memo.check_memo = checkbox_state is not None  # チェックされているかどうかを設定
+        db.session.commit()
+        return redirect(url_for('veiw', id=id))
+    else:
+        return render_template('veiw.html', todo=todo, tags=tags, memos=memos)
+    
     
 @app.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
     if request.method == 'POST':
-        do = request.form.get('do')
-        memo = request.form.get('memo')
+        do = request.form.get('title')
         day = request.form.get('day')
-        
-        important = request.form.get('important') is not None
-        uni = request.form.get('uni') is not None
-        private = request.form.get('private') is not None
-        task = request.form.get('task') is not None
+        time = request.form.get('time')
         
         if not do or not day:
             error = "記入していない項目があります"
             return render_template('create.html', error=error)
         
+        if Todo.query.filter_by(do = do).first():
+            error = "使われているタイトル名です"
+            return render_template('create.html', error=error, do=do)
+        
         try:
             day = datetime.strptime(day, '%Y-%m-%d').date()  # 日付文字列をdateオブジェクトに変換
-        except ValueError:
-            error = "無効な日付形式です"
+            
+            if time:
+                time = datetime.strptime(time, '%H:%M').time()
+                # 日付と時間を組み合わせて datetime オブジェクトに変換
+                days = datetime.combine(day, time).replace(second=0)
+            else:
+                # 時間が指定されていない場合、デフォルトで00:00:00を使用
+                days = datetime.combine(day, datetime.min.time()).replace(second=0)
+        except ValueError as e: #ValueError:
+            error = f"無効な日付、時間形式です: {str(e)}" #"無効な日付、時間形式です"
             return render_template('create.html', error=error)
         
-        new_create = Todo(do=do, memo=memo, day=day, User_link_id=current_user.id, important=important, uni=uni, private=private, task=task)
-
+        new_create = Todo(do=do, day=days, User_link_id=current_user.id)
         db.session.add(new_create)
         db.session.commit()
         
+        form_data = request.form
+        
+        memos = []
+        memo_count = len([key for key in form_data if key.startswith('memo')])
+        for i in range(1, memo_count + 1):
+            memo_content = form_data.get(f'memo{i}')
+            if memo_content:
+                memo = Memo(content=memo_content, Todo_link_id=new_create.id)
+                memos.append(memo)
+
+        if memos:
+            db.session.add_all(memos)
+        
+        #タグの処理
+        tags = []
+        tag_count = len([key for key in form_data if key.startswith('tag_name')])
+        for i in range(1, tag_count + 1):
+            tag_name = form_data.get(f'tag_name{i}')
+            tag_color = form_data.get(f'tag_color{i}')
+            
+            if tag_name and tag_color:
+                color_map = {
+                    "黒": "black",
+                    "赤": "red",
+                    "緑": "green",
+                    "黄": "yellow",
+                    "青": "blue"
+                }
+                tag_color = color_map.get(tag_color, tag_color) #漢字を英語に変換(color_mapに記載されているように変換)
+                
+                user_id = current_user.id
+                todo_id = Todo.query.filter_by(do = do).first()
+                tag = Tag(tag_name=tag_name, tag_color=tag_color, User_link_id=user_id, Todo_link_id=todo_id.id)
+                
+                tags.append(tag)
+            else:
+                error = "タグ名か色が記入されていません"
+                
+                delete_todo = Todo.query.filter_by(do = do).first()
+                memos = Memo.query.filter_by(Todo_link_id=delete_todo.id).all()
+                
+                for memo in memos:
+                    db.session.delete(memo)
+                db.session.delete(delete_todo)
+                
+                db.session.commit()
+                
+                return render_template('create.html', error=error, do=do)
+        
+        
+        db.session.add_all(tags)
+        
+        db.session.commit()
+        
         return redirect('/home')
+    
+        
 
     else:
         return render_template('create.html')
@@ -200,17 +280,17 @@ def create():
 @app.route('/<int:id>/update', methods=['GET', 'POST'])
 @login_required
 def update(id):
-    todo=Todo.query.get(id)
+    todo = Todo.query.get(id)
+    tags = Tag.query.filter_by(User_link_id=current_user.id, Todo_link_id=id).all()
+    memos = Memo.query.filter_by(Todo_link_id=id).all()
     
     if request.method == 'POST':
-        do = request.form.get('do')
-        memo = request.form.get('memo')
-        day = request.form.get('day')
+        form_data = request.form
         
-        important = request.form.get('important') is not None
-        uni = request.form.get('uni') is not None
-        private = request.form.get('private') is not None
-        task = request.form.get('task') is not None
+        do = form_data.get('title')
+        day = form_data.get('day')
+        time = form_data.get('day_time')
+        
     
         if not do or not day:
                 error = "記入していない項目があります"
@@ -218,32 +298,118 @@ def update(id):
             
         try:
             day = datetime.strptime(day, '%Y-%m-%d').date()  # 日付文字列をdateオブジェクトに変換
-        except ValueError:
-            error = "無効な日付形式です"
-            return render_template('create.html', todo=todo, error=error)
+            
+            if time:
+                time = datetime.strptime(time, '%H:%M').time()
+                # 日付と時間を組み合わせて datetime オブジェクトに変換
+                days = datetime.combine(day, time).replace(second=0)
+            else:
+                # 時間が指定されていない場合、デフォルトで00:00:00を使用
+                days = datetime.combine(day, datetime.min.time()).replace(second=0)
+        except ValueError as e: #ValueError:
+            error = f"無効な日付、時間形式です: {str(e)}" #"無効な日付、時間形式です"
+            return render_template('create.html', error=error)
         
         if do:
             todo.do = do
-        if memo:
-            todo.memo = memo
         if day:
-            todo.day = day
+            todo.day = days
+        
+        for tag in tags:
+            tag_name = form_data.get(f'tag_name{tag.id}')
+            tag_color = form_data.get(f'tag_color{tag.id}')
             
-        todo.important = important
-        todo.uni = uni
-        todo.private = private
-        todo.task = task
+            
+            if tag_name and tag_color:
+                color_map = {
+                    "黒": "black",
+                    "赤": "red",
+                    "緑": "green",
+                    "黄": "yellow",
+                    "青": "blue"
+                }
+                tag_color = color_map.get(tag_color, tag_color) #漢字を英語に変換(color_mapに記載されているように変換)
+                
+            tag.tag_name = tag_name
+            tag.tag_color = tag_color
+            
+        #tagsの数を超えた分をdbに保存(add)
+        new_tags = []
+        now_tag_count=len(tags)
+        tag_count = len([key for key in form_data if key.startswith('tag_name')])
+        for i in range(1, tag_count-now_tag_count +1):
+            new_tag_name = form_data.get(f'tag_name{i}')
+            new_tag_color = form_data.get(f'tag_color{i}')
+                
+            if new_tag_name and new_tag_color:
+                color_map = {
+                    "黒": "black",
+                    "赤": "red",
+                    "緑": "green",
+                    "黄": "yellow",
+                    "青": "blue"
+                }
+                new_tag_color = color_map.get(new_tag_color, new_tag_color) #漢字を英語に変換(color_mapに記載されているように変換)
+                
+                user_id = current_user.id
+                todo_id = Todo.query.filter_by(do = do).first()
+                tag = Tag(tag_name=new_tag_name, tag_color=new_tag_color, User_link_id=user_id, Todo_link_id=todo_id.id)
+                
+                new_tags.append(tag)
+            else:
+                error = "タグ名か色が記入されていません"
+                return redirect('/<int:id>/update')
+        
+        db.session.add_all(new_tags)
+        
+        #memosの数を超えた分をdbに保存
+        new_memos = []
+        now_memo_count=len(memos)
+        memo_count = len([key for key in form_data if key.startswith('memo')])
+        for i in range(1, now_memo_count-memo_count +1):
+            new_memo = form_data.get(f'memo{i}')
+            
+            if new_memo:
+                memo = Memo(content=new_memo, Todo_link_id=id)
+                new_memos.append(memo)
+                
+        if new_memos:
+            db.session.add_all(new_memos)
             
         db.session.commit()
         return redirect('/home')
 
     else:
-        return render_template('update.html', todo=todo)
+        for tag in tags:
+            tag_name = tag.tag_name
+            tag_color = tag.tag_color
+            
+            tag_count=0
+            if tag_name and tag_color:
+                color_map = {
+                    "black": "黒",
+                    "red": "赤",
+                    "green": "緑",
+                    "yellow": "黄",
+                    "blue": "青"
+                }
+                tag_color = color_map.get(tag_color, tag_color) #英語を感じに変換(color_mapに記載されているように変換)
+                tag.tag_color = color_map.get(tag_color, tag_color)
+                
+        return render_template('update.html', todo=todo, tags=tags, memos=memos)
     
 @app.route('/<int:id>/delete', methods=['GET'])
 @login_required
 def delete(id):
     todo = Todo.query.get(id)
+    tags = Tag.query.filter_by(Todo_link_id=id).all()
+    memos = Memo.query.filter_by(Todo_link_id=id).all()
+    
+    for tag in tags:
+        db.session.delete(tag)
+
+    for memo in memos:
+        db.session.delete(memo)
 
     db.session.delete(todo)
     db.session.commit()
@@ -261,5 +427,3 @@ def logout():
 if __name__ == '__main__':
     app.debug = True
     app.run()
-    
-    
